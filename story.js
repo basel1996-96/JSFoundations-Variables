5a7d0df09d782051bf289ebd2ef0028e525d22ca
Basel Abu Tarboush <babutarboush@premierlending.com>
2021-05-17 05:35:20 -0700

    first

---
+++ story.js
@@ -0,0 +1,4 @@
+let name = "Basel"
+let age = 25 
+let hobby = "football" 
+console.log (`Mr. ${name} in 33 years, you will be ${age+33} years old. By then you'll have decades of ${hobby} practice`)
